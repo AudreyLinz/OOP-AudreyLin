85009472f2e07c4a65aac118b1a2b8acff5a30d6
AudreyLinz <alinfors7396@floridapoly.edu>
2025-08-25 14:46:25 +0000

    saving

---
+++ OOP1/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float grade;
+    cout << "Enter a Grade: ";
+    cin >> grade;
+    cout << "U entered " << grade << endl;
+    return 0;
+}
---
+++ OOP1/practice1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime> //
+
+
+using namespace std;
+
+int main(){
+    int day = 2;
+    switch (day){
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "tuesday";
+        case 3:
+            cout << "Wesnday";
+    }
+
+
+
+//std::cout << (10.5 + 2 * 3);
+//cout << "(10.5 + 2 * 3) / (45 - 3.5) = " << endl;
+//cout << (10.5 + 2 * 3) / (45 - 3.5) << endl;
+return 0;
+}
+
+//notes
+/*
+cplusplus.com is good recourse
+
+git clone https://github.com/johndoe/my-cpp-project.git
+cd my-cpp-project
+g++ hello.cpp -o hello
+./hello
+
+*/
